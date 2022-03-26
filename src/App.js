2b300769dfc8e008cdb779c0e23239bf3301f0e0
joseburgosguntin <josem.123burgos@gmail.com>
2022-03-25 23:51:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+const { useState, useEffect } = require("react");
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [count, setCount] = useState(0)
+    const [loaded, setLoaded] = useState(false)
+    useEffect(() => {
+        alert('hello side effect!')
+
+        return () => alert("goodbye component!")
+    })
+
+    useEffect(() => {
+        fetch('foo').then(() => setLoaded(true) )
+    },
+    [count]
+    )
+
+    return (
+        <button onClick={() => setCount(count + 1)}>
+        {count}
+    </button>) 
 }
 
 export default App;
